ef2c309f111fab1f03bb159e5c6e4dd58b6d5dab
pablocoacci <pablocor85@hotmail.com>
2014-10-11 18:11:18 -0300

    Pistas 6,7,8
    Agregue las pistas 6,7 y 8 con sus respectivos testcase

---
+++ AlgoritmosGeneticos/Domain/Pistas/Pista6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmosGeneticos.Domain.Pistas
+{
+    public class Pista6 : Pista
+    {
+        public Pista6() { }
+
+        public double validar(List<Modelo> modelos)
+        {
+            int valorRetorno = -1;
+            List<Modelo> mods_bordo = modelos.Where(x => x.color.ToLower() == "bordo").ToList();
+
+            foreach (Modelo mod in mods_bordo)
+            {
+                List<Modelo> mod_derecha = modelos.Where(x => x.posicion == mod.posicion + 1).ToList();
+
+                if (mod_derecha != null && mod_derecha.Exists(x => x.pertenencia.ToLower() == "notebook"))
+                    valorRetorno = 1;
+
+                List<Modelo> mod_izq = modelos.Where(x => x.posicion == mod.posicion -1).ToList();
+
+                if (mod_izq != null && mod_izq.Exists(x => x.pertenencia.ToLower() == "notebook"))
+                    valorRetorno = 1;
+            }
+
+            return valorRetorno;
+        }
+    }
+}

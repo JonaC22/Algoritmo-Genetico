a8d1b86a13d78c60da1a3b48ce2e2481123ab722
pablocoacci <pablocor85@hotmail.com>
2014-10-11 19:50:59 -0300

    Agregue las pistas a acertijo

--- AlgoritmosGeneticos/Domain/Acertijo.cs
+++ AlgoritmosGeneticos/Domain/Acertijo.cs
@@ -38,9 +38,12 @@ namespace AlgoritmosGeneticos.Domain
             pistas.Add(new Pista2());
             pistas.Add(new Pista3());
             pistas.Add(new Pista4());
+            pistas.Add(new Pista5());
             pistas.Add(new Pista6());
             pistas.Add(new Pista7());
             pistas.Add(new Pista8());
+            pistas.Add(new Pista9());
+            pistas.Add(new Pista10());
             pistas.Add(new Pista11());
             pistas.Add(new Pista12());
         }
